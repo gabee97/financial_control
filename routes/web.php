424027891a03c27f8951f2b5bf8f6c801f1ac9db
= <=>
2024-12-12 11:21:37 -0300

    Removida redundancia da rota

--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,41 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\TransactionController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| Register web routes for the application. These routes are loaded by the
+| RouteServiceProvider within a group which contains the "web" middleware group.
 |
 */
 
+// Página inicial
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Dashboard - Protegido por autenticação
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Rotas protegidas por autenticação
 Route::middleware('auth')->group(function () {
+    // Rotas de perfil
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    // Rotas de categorias e transações
     Route::resource('categories', CategoryController::class);
     Route::resource('transactions', TransactionController::class);
-    Route::resource('categories/create', CategoryController::class);
-    Route::resource('categories', CategoryController::class);
 });
 
-
+// Rotas de autenticação (Breeze)
 require __DIR__.'/auth.php';
