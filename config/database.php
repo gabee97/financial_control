25d793cd91a03af9b8525c427505c3b054d7af5b
Gabriel <gaabriel123@hotmail.com>
2024-12-17 22:40:34 -0300

    Alterada config de database

--- config/database.php
+++ config/database.php
@@ -45,22 +45,21 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => '127.0.0.1',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'unix_socket' => env('DB_SOCKET', ''), // Confirme que está vazio
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'options' => [
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
+            ],
         ],
 
         'pgsql' => [
